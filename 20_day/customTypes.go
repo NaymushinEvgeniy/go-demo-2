cfcd8bf05219406a046079c98bb15262e407ea50
Evgeniy MacMini <evgeniy@naymushin.xyz>
2024-05-27 17:53:39 +0300

    ops with custom types

--- 20_day/customTypes.go
+++ 20_day/customTypes.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"reflect"
 )
 
 type Liters float64
@@ -15,4 +16,25 @@ func main() {
 	busFuel = Liters(240.0)
 	fmt.Println(carFuel, busFuel)
 
+	var carFuelTwo Gallons
+	var busFuelTwo Liters
+	carFuelTwo = 10.0
+	busFuelTwo = 240.0
+	fmt.Println(carFuelTwo, busFuelTwo)
+
+	fmt.Println(reflect.TypeOf(carFuel))
+	fmt.Println(reflect.TypeOf(carFuelTwo))
+
+	var carFuelTree Gallons
+	var busFuelTree Liters
+	carFuelTree = Gallons(Liters(40.0) * 0.264)
+	busFuelTree = Liters(Gallons(63.0) * 3.785)
+	fmt.Println(carFuelTree)
+	fmt.Println(reflect.TypeOf(carFuelTree))
+	fmt.Println(busFuelTree)
+	fmt.Println(reflect.TypeOf(busFuelTree))
+
+	fmt.Println(Gallons(10.2) + Gallons(22.2))
+	fmt.Println(Liters(20.2) * Liters(2.2))
+	fmt.Println(Liters(1.2) == 1.2)
 }
