8ad0eb6449624fbf5d7ca2867d4db3d97e4c95c6
Evgeniy MacMini <evgeniy@naymushin.xyz>
2024-05-30 17:09:49 +0300

    add validation

--- incups.go
+++ incups.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"errors"
 	"fmt"
+	"log"
 )
 
 type Date struct {
@@ -10,27 +12,46 @@ type Date struct {
 	Year  int
 }
 
-func (d *Date) SetYear(year int) {
+func (d *Date) SetYear(year int) error {
+	if year < 1 {
+		return errors.New("invalid year")
+	}
 	d.Year = year
+	return nil
 }
 
-func (d *Date) SetMonth(month int) {
+func (d *Date) SetMonth(month int) error {
+	if month < 1 || month > 12 {
+		return errors.New("invalid month")
+	}
 	d.Month = month
+	return nil
 }
 
-func (d *Date) SetDay(day int) {
+func (d *Date) SetDay(day int) error {
+	if day < 1 || day > 31 {
+		return errors.New("invalid day")
+	}
 	d.Day = day
+	return nil
 }
 
 func main() {
 	dayOne := Date{}
-	dayOne.SetYear(2002)
-	dayOne.SetMonth(11)
-	dayOne.SetDay(2)
-	dayTwo := Date{}
-	dayTwo.SetYear(1992)
-	dayTwo.SetMonth(2)
-	dayTwo.SetDay(11)
-	fmt.Println(dayOne)
-	fmt.Println(dayTwo)
+	err := dayOne.SetYear(2002)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(dayOne.Year)
+	errTwo := dayOne.SetMonth(11)
+	if errTwo != nil {
+		log.Fatal(errTwo)
+	}
+	fmt.Println(dayOne.Month)
+	errTree := dayOne.SetDay(2)
+	if errTree != nil {
+		log.Fatal(errTree)
+	}
+	fmt.Println(dayOne.Month)
+
 }
