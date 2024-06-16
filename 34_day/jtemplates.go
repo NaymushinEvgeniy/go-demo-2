55625c995ee8c117db582dce01a8957143c88055
Evgeniy Macbook <evgeniy@naymushin.xyz>
2024-06-16 22:20:52 +0300

    loop

--- 34_day/jtemplates.go
+++ 34_day/jtemplates.go
@@ -12,13 +12,18 @@ func check(err error) {
 	}
 }
 
+func executeTmpl(text string, data interface{}) {
+	tmpl, err := template.New("test").Parse(text)
+	check(err)
+	err = tmpl.Execute(os.Stdout, data)
+	check(err)
+}
+
 func main() {
 	myTemplate := "Start of template\n Value of template: {{.}}\n End of template\n"
-	tmpl, err := template.New("test").Parse(myTemplate)
-	check(err)
-	err = tmpl.Execute(os.Stdout, "ABC")
-	check(err)
-	err = tmpl.Execute(os.Stdout, false)
-	check(err)
-	err = tmpl.Execute(os.Stdout, 344334)
+	executeTmpl(myTemplate, "ABV")
+	executeTmpl("This is dot {{ if . }}Yes is true!{{end}}\n", true)
+	executeTmpl("This is dot {{ if . }}Yes is true!{{end}}\n", false)
+	exLoop := "This is loop {{ . }}\n in loop:\n {{ range .}} value: {{.}}\n {{ end}} After loop :{{ . }}\n"
+	executeTmpl(exLoop, []string{"do", "re", "mi"})
 }
